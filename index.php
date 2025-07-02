433c708b0a3a2968e47d377b9b468af3d51f32df
Eduardo <edu.locatelli2004@gmail.com>
2025-07-02 11:37:28 -0300

    resolvi de novo

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php 
 
-<<<<<<< HEAD
     echo "Olá mundo 456!";
 
-    echo "Olá udemy!";
-=======
-    echo "Olá mundo 123!";
-
-    echo "Olá Hcode!";
->>>>>>> 4e466fac17d1d5d12e66b375368d04c1f26411d3
+    echo "Olá udemy e Hcode!"; 
 
 ?>
